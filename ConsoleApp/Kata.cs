bc895f60708bcbfe8c69636ca7b626aea1c15f75
ChuckLu <244657538@qq.com>
2016-08-23 10:51:16 +0800

    https://www.codewars.com/kata/correct-the-mistakes-of-the-character-recognition-software/train/csharp

--- ConsoleApp/Kata.cs
+++ ConsoleApp/Kata.cs
@@ -8,5 +8,14 @@ namespace ConsoleApp
 {
     public class Kata
     {
+        /// <summary>
+        /// https://www.codewars.com/kata/correct-the-mistakes-of-the-character-recognition-software/train/csharp
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string Correct(string text)
+        {
+            return text.Replace("5", "S").Replace("1", "I").Replace("0", "O");
+        }
     }
 }
