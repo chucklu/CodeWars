fb589434e34c7cfbd6dad384eeeee7bc9bb605d5
ChuckLu <244657538@qq.com>
2016-08-23 09:15:16 +0800

    Main函数中增加了捕获并处理异常的代码

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                while (ex != null)
+                {
+                    Console.WriteLine("错误信息:{0}{1}异常类型:{2}{1}程序集:{3}{1}方法:{4}{1}跟踪栈信息:{1}{5}{1}",
+                        ex.Message, Environment.NewLine, ex.GetType(), ex.Source, ex.TargetSite, ex.StackTrace);
+                    ex = ex.InnerException;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
