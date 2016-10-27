1be150873a06d4066424c475895f25676a6b1df1
ChuckLu <244657538@qq.com>
2016-10-27 21:04:44 +0800

    https://www.codewars.com/kata/numbers-to-letters/train/csharp

--- ConsoleApp/Kata.cs
+++ ConsoleApp/Kata.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 /// <summary>
-/// https://www.codewars.com/kata/get-number-from-string/train/csharp
+/// https://www.codewars.com/kata/numbers-to-letters/train/csharp
 /// </summary>
-public static class Kata
+public class Kata
 {
-    public static int getNumberFromString(string s)
+    private static readonly Dictionary<int, char> dictionary;
+
+    static Kata()
     {
-        return Convert.ToInt32(string.Join(string.Empty, s.Where(char.IsDigit)));
+        dictionary = new Dictionary<int, char>();
+        int number = 26;
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            dictionary.Add(number--, c);
+        }
+        dictionary.Add(27, '!');
+        dictionary.Add(28, '?');
+        dictionary.Add(29, ' ');
+    }
+
+    public static string Switcher(string[] array)
+    {
+        return string.Concat(array.Select(n => dictionary[Convert.ToInt32(n)]));
     }
 }
--- ConsoleApp/KataTests.cs
+++ ConsoleApp/KataTests.cs
@@ -4,11 +4,33 @@
 public class KataTests
 {
     [Test]
-    public void BasicTests()
+    public void ExampleTest1()
     {
-        Assert.AreEqual(1, Kata.getNumberFromString("1"));
-        Assert.AreEqual(123, Kata.getNumberFromString("123"));
-        Assert.AreEqual(7, Kata.getNumberFromString("this is number: 7"));
+        string expected = "codewars";
+
+        string actual = Kata.Switcher(new string[] { "24", "12", "23", "22", "4", "26", "9", "8" });
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void ExampleTest2()
+    {
+        string expected = "btswmdsbd kkw";
+
+        string actual = Kata.Switcher(new string[] { "25", "7", "8", "4", "14", "23", "8", "25", "23", "29", "16", "16", "4" });
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void ExampleTest3()
+    {
+        string expected = "wc";
+
+        string actual = Kata.Switcher(new string[] { "4", "24" });
+
+        Assert.AreEqual(expected, actual);
     }
 }
 
