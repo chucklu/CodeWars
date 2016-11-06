4c3cdbd71b23790d468cc82709d0fbf740ec6616
ChuckLu <244657538@qq.com>
2016-11-06 20:54:19 +0800

    https://www.codewars.com/kata/help-bob-count-letters-and-digits/train/csharp

--- ConsoleApp/Kata.cs
+++ ConsoleApp/Kata.cs
@@ -1,38 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 /// <summary>
-/// https://www.codewars.com/kata/correct-the-time-string/train/csharp
+/// https://www.codewars.com/kata/help-bob-count-letters-and-digits/train/csharp
 /// </summary>
-public static class Time
+public static class Kata
 {
-    public static string Correct(string timeString)
+    public static int CountLettersAndDigits(string input)
     {
-        if (string.IsNullOrEmpty(timeString))
-        {
-            return timeString;
-        }
-        var temp = timeString.Split(':');
-        if (temp.Length != 3)
-        {
-            return null;
-        }
-        List<int> array;
-        try
-        {
-            array = temp.Select(x => Convert.ToInt32(x)).ToList();
-        }
-        catch
-        {
-            return null;
-        }
-
-        DateTime time = DateTime.MinValue;
-        time = time.AddHours(array[0]);
-        time = time.AddMinutes(array[1]);
-        time = time.AddSeconds(array[2]);
-        string str = $"{time.Hour:D2}:{time.Minute:D2}:{time.Second:D2}";
-        return str;
+        return input.Count(char.IsLetterOrDigit);
     }
 }
--- ConsoleApp/KataTests.cs
+++ ConsoleApp/KataTests.cs
@@ -4,33 +4,9 @@
 public class KataTests
 {
     [Test]
-    public void NullTest()
+    public void Test1()
     {
-        Assert.AreEqual(null, Time.Correct(null));
-    }
-
-    [Test]
-    public void EmptyTest()
-    {
-        Assert.AreEqual("", Time.Correct(""));
-    }
-
-    [Test]
-    public void InvalidFormatTests()
-    {
-        Assert.AreEqual(null, Time.Correct("001122"));
-        Assert.AreEqual(null, Time.Correct("00;11;22"));
-        Assert.AreEqual(null, Time.Correct("0a:1c:22"));
-    }
-
-    [Test]
-    public void CorrectionTests()
-    {
-        Assert.AreEqual("09:10:01", Time.Correct("09:10:01"));
-        Assert.AreEqual("12:10:10", Time.Correct("11:70:10"));
-        Assert.AreEqual("20:40:39", Time.Correct("19:99:99"));
-        Assert.AreEqual("00:01:01", Time.Correct("24:01:01"));
-        Assert.AreEqual("04:01:01", Time.Correct("52:01:01"));
+        Assert.AreEqual(7, Kata.CountLettersAndDigits("n!!ice!!123"));
     }
 }
 
