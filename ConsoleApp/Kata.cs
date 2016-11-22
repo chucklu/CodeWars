a06c56581ea5f5a40cb19341c2ca1258658c4fda
ChuckLu <244657538@qq.com>
2016-11-22 20:07:03 +0800

    https://www.codewars.com/kata/the-dropwhile-function/train/csharp

--- ConsoleApp/Kata.cs
+++ ConsoleApp/Kata.cs
@@ -1,17 +1,19 @@
-﻿/// <summary>
-/// https://www.codewars.com/kata/recursion-number-1-factorial/train/csharp
+﻿using System;
+using System.Linq;
+
+/// <summary>
+/// https://www.codewars.com/kata/the-dropwhile-function/train/csharp
 /// </summary>
-public class Recursion
+public class Kata
 {
-    public static ulong Factorial(ulong n)
+    public static int[] DropWhile(int[] arr, Func<int, bool> pred)
     {
-        checked
+        var list = arr.ToList();
+        var index = list.FindIndex(x => !pred(x));
+        if (index == -1)
         {
-            if (n == 0||n==1)
-            {
-                return 1;
-            }
-            return n*Factorial(n - 1);
+            return new int[] { };
         }
+        return list.Skip(index).Take(list.Count - index).ToArray();
     }
 }
--- ConsoleApp/KataTests.cs
+++ ConsoleApp/KataTests.cs
@@ -1,18 +1,39 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 [TestFixture]
 public class KataTests
 {
     [TestFixture]
-    public class Test
+    public class SolutionTest
     {
+        private Func<int, bool> isEven = (value) => value % 2 == 0;
+
         [Test]
-        public void BasicTests()
+        public void BasicTest1()
         {
-            Assert.AreEqual(1, Recursion.Factorial(0));
-            Assert.AreEqual(1, Recursion.Factorial(1));
-            Assert.AreEqual(2, Recursion.Factorial(2));
-            Assert.AreEqual(6, Recursion.Factorial(3));
+            var expected = new int[] { 1, 5, 4, 3 };
+            var actual = Kata.DropWhile(new int[] { 2, 6, 4, 10, 1, 5, 4, 3 }, isEven);
+
+            Assert.AreEqual(string.Join(", ", expected), string.Join(", ", actual));
+        }
+
+        [Test]
+        public void BasicTest2()
+        {
+            var expected = new int[] { 5, 3, 4, 6 };
+            var actual = Kata.DropWhile(new int[] { 2, 100, 1000, 10000, 5, 3, 4, 6 }, isEven);
+
+            Assert.AreEqual(string.Join(", ", expected), string.Join(", ", actual));
+        }
+
+        [Test]
+        public void BasicTest3()
+        {
+            var expected = new int[] { };
+            var actual = Kata.DropWhile(new int[] { 2, 4, 10, 100, 64, 78, 92 }, isEven);
+
+            Assert.AreEqual(string.Join(", ", expected), string.Join(", ", actual));
         }
     }
 }
