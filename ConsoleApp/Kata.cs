fe8120e932c01a09107fcbecbd940d2736d711af
ChuckLu <244657538@qq.com>
2016-11-22 20:09:48 +0800

    使用SkipWhile

--- ConsoleApp/Kata.cs
+++ ConsoleApp/Kata.cs
@@ -8,12 +8,6 @@ public class Kata
 {
     public static int[] DropWhile(int[] arr, Func<int, bool> pred)
     {
-        var list = arr.ToList();
-        var index = list.FindIndex(x => !pred(x));
-        if (index == -1)
-        {
-            return new int[] { };
-        }
-        return list.Skip(index).Take(list.Count - index).ToArray();
+        return arr.SkipWhile(pred).ToArray();
     }
 }
