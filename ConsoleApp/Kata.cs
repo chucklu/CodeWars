428110fc1eb2bd5b8f9476f432eee3fe6a26e042
ChuckLu <244657538@qq.com>
2016-12-02 20:28:28 +0800

    https://www.codewars.com/kata/complete-the-pattern-number-4/train/csharp

--- ConsoleApp/Kata.cs
+++ ConsoleApp/Kata.cs
@@ -1,14 +1,14 @@
-﻿/// <summary>
-/// https://www.codewars.com/kata/factorial-1/train/csharp
+﻿using System.Linq;
+
+/// <summary>
+/// https://www.codewars.com/kata/complete-the-pattern-number-4/train/csharp
 /// </summary>
 public class Kata
 {
-    public static int factorial(int n)
+    public static string Pattern(int n)
     {
-        if (n == 0 || n == 1)
-        {
-            return 1;
-        }
-        return n*factorial(n - 1);
+        if (n <= 0)
+            return string.Empty;
+        return string.Join("\n", Enumerable.Range(1, n).Select(i => string.Concat(Enumerable.Range(i, n - i + 1))));
     }
 }
--- ConsoleApp/KataTests.cs
+++ ConsoleApp/KataTests.cs
@@ -6,12 +6,9 @@ public class KataTests
     [Test]
     public void BasicTests()
     {
-        Assert.AreEqual(1, Kata.factorial(0));
-        Assert.AreEqual(1, Kata.factorial(1));
-        Assert.AreEqual(2, Kata.factorial(2));
-        Assert.AreEqual(6, Kata.factorial(3));
-        Assert.AreEqual(24, Kata.factorial(4));
-        Assert.AreEqual(120, Kata.factorial(5));
+        Assert.AreEqual("1", Kata.Pattern(1));
+        Assert.AreEqual("12\n2", Kata.Pattern(2));
+        Assert.AreEqual("12345\n2345\n345\n45\n5", Kata.Pattern(5));
     }
 }
 
