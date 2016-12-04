1f74bf8f57490cf8b2d004cc604908d53ca9827d
ChuckLu <244657538@qq.com>
2016-12-04 20:55:10 +0800

    https://www.codewars.com/kata/sort-arrays-1/train/csharp

--- ConsoleApp/Kata.cs
+++ ConsoleApp/Kata.cs
@@ -1,21 +1,13 @@
 ﻿using System;
 
 /// <summary>
-/// https://www.codewars.com/kata/unlucky-days/train/csharp
+/// https://www.codewars.com/kata/sort-arrays-1/train/csharp
 /// </summary>
 public class Kata
 {
-    public static int UnluckyDays(int year)
+    public static string[] SortMe(string[] names)
     {
-        int count = 0;
-        for (int i = 1; i <= 12; i++)
-        {
-            var dateTime = new DateTime(year, i, 13);
-            if (dateTime.DayOfWeek == DayOfWeek.Friday)
-            {
-                count++;
-            }
-        }
-        return count;
+        Array.Sort(names);
+        return names;
     }
 }
--- ConsoleApp/KataTests.cs
+++ ConsoleApp/KataTests.cs
@@ -6,16 +6,7 @@ public class KataTests
     [Test]
     public void BasicTests()
     {
-        Assert.AreEqual(1, Kata.UnluckyDays(1586));
-        Assert.AreEqual(3, Kata.UnluckyDays(1001));
-        Assert.AreEqual(2, Kata.UnluckyDays(2819));
-        Assert.AreEqual(2, Kata.UnluckyDays(2792));
-        Assert.AreEqual(2, Kata.UnluckyDays(2723));
-        Assert.AreEqual(1, Kata.UnluckyDays(1909));
-        Assert.AreEqual(2, Kata.UnluckyDays(1812));
-        Assert.AreEqual(2, Kata.UnluckyDays(1618));
-        Assert.AreEqual(1, Kata.UnluckyDays(2132));
-        Assert.AreEqual(3, Kata.UnluckyDays(2065));
+        Assert.AreEqual("one,three,two", string.Join(",", Kata.SortMe(new[] { "one", "two", "three" })));
     }
 }
 
