7a99c40a34fa7b58661bed73dfa871569746fda1
ChuckLu <244657538@qq.com>
2016-12-17 20:14:22 +0800

    https://www.codewars.com/kata/ipv4-validator/train/csharp

--- ConsoleApp/Kata.cs
+++ ConsoleApp/Kata.cs
@@ -1,71 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Net;
 
 /// <summary>
-/// https://www.codewars.com/kata/pokerhand-straight-or-not/train/csharp
+/// https://www.codewars.com/kata/ipv4-validator/train/csharp
 /// </summary>
-public static class PokerHand
+public class Kata
 {
-    public static bool IsStraight(List<Card> cards)
+    public static bool IpValidator(string ip)
     {
-        bool flag = false;
-        if (cards.Count < 5)
-        {
-            return flag;
-        }
-        var array = cards.Select(Ace).OrderBy(x => x).ToArray();
-        flag = Is5Order(array);
-        if (flag == false)
-        {
-            array = cards.Select(x => x.CardValue).OrderBy(x => x).ToArray();
-            flag = Is5Order(array);
-        }
-        return flag;
+        if (ip.Count(c => c == '.') != 3) return false;
+        IPAddress address;
+        return IPAddress.TryParse(ip, out address);
     }
-
-    private static bool Is5Order(int[] array)
-    {
-        for (int i = 0; i < array.Length - 1; i++)
-        {
-            int count = 0;
-            for (int j = i; j < array.Length - 1; j++)
-            {
-                if (array[j] + 1 == array[j + 1])
-                {
-                    count++;
-                    if (count == 4)
-                    {
-                        return true;
-                    }
-                }
-                else if (array[j] == array[j + 1])
-                {
-                }
-                else
-                {
-
-                    if (count < 4)
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private static int Ace(Card card)
-    {
-        if (card.CardValue == 14)
-        {
-            return 1;
-        }
-        return card.CardValue;
-    }
-}
-
-public class Card
-{
-    public int CardValue { get; set; } // 2-14
 }
--- ConsoleApp/KataTests.cs
+++ ConsoleApp/KataTests.cs
@@ -1,29 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 [TestFixture]
 public class KataTests
 {
-    [TestCase(new[] { 2, 3, 4, 5, 6 }, true)]
-    [TestCase(new[] { 14, 5, 4, 2, 3 }, true)]
-    [TestCase(new[] { 7, 7, 12, 11, 3, 4, 14 }, false)]
-    [TestCase(new[] { 7, 3, 2 }, false)]
-    public void IsStraight(IEnumerable<int> cardValues, bool isStraight)
+    [TestCase("123.456.789.0", false)]
+    [TestCase("127.0.0.1", true)]
+    public void IpValidatorTest(string ip, bool isValid)
     {
-        var cards = new List<Card>();
-        if (cardValues == null)
-        {
-            throw  new ArgumentNullException(nameof(cardValues));
-        }
-        var list = cardValues.ToList();
-        foreach (var cardValue in list)
-        {
-            cards.Add(new Card { CardValue = cardValue });
-         }
-
-        Assert.AreEqual(isStraight, PokerHand.IsStraight(cards), string.Join(", ", list.Select(v => v)));
+        Assert.AreEqual(isValid, Kata.IpValidator(ip));
     }
 }
 
