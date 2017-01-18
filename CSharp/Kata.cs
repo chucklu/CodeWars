df5c7a54d2c948bd6400bc13d12d14e7bfc0a8c2
ChuckLu <244657538@qq.com>
2017-01-18 20:44:35 +0800

    https://www.codewars.com/kata/adding-values-of-arrays-in-a-shifted-way/train/csharp

--- CSharp/Kata.cs
+++ CSharp/Kata.cs
@@ -1,13 +1,51 @@
-﻿using System.Linq;
-
-/// <summary>
-/// https://www.codewars.com/kata/disemvowel-trolls/train/csharp
+﻿/// <summary>
+/// https://www.codewars.com/kata/adding-values-of-arrays-in-a-shifted-way/train/csharp
 /// </summary>
 public static class Kata
 {
-    public static string Disemvowel(string str)
+    public static int[] AddingShifted(int[][] arrayOfArrays, int shift)
     {
-        return string.Join(string.Empty, str.Where(c => "aeiouAEIOU".IndexOf(c) == -1));
+        var row = arrayOfArrays.GetLength(0);
+        var originColumn = arrayOfArrays[0].Length;
+        int column = shift*(row - 1) + originColumn;
+        var tempArrayOfArrays = new int[row][];
+        for (int i = 0; i < row; i++)
+        {
+            var begin = shift*i;
+            tempArrayOfArrays[i] = GetArray(begin, column, arrayOfArrays[i]);
+        }
+
+        var temp = new int[column];
+        for (int i = 0; i < column; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                temp[i] = temp[i] + tempArrayOfArrays[j][i];
+            }
+        }
+        return temp;
+    }
+
+    private static int[] GetArray(int begin,int total,int[] tempArray)
+    {
+        var array = new int[total];
+        for (int i = 0; i < total; i++)
+        {
+            if (i < begin)
+            {
+                continue;
+            }
+            if (i - begin < tempArray.Length)
+            {
+
+                array[i] = tempArray[i - begin];
+            }
+            else
+            {
+                break;
+            }
+        }
+        return array;
     }
 }
 
--- CSharp/KataTests.cs
+++ CSharp/KataTests.cs
@@ -6,7 +6,23 @@ public class KataTests
     [Test]
     public void BasicTests()
     {
-        Assert.AreEqual(Kata.Disemvowel("This website is for losers LOL!"),"Ths wbst s fr lsrs LL!");
+        var arrayOfArrays = new int[][] { new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 7, 7, 7, 7, 7, -7 } };
+        var expected = new int[] { 8, 9, 10, 11, 12, -1 };
+        var result = Kata.AddingShifted(arrayOfArrays, 0);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(string.Join(",", expected), string.Join(",", result));
+
+        arrayOfArrays = new int[][] { new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 7, 7, 7, 7, 7, 7 } };
+        expected = new int[] { 1, 2, 3, 11, 12, 13, 7, 7, 7 };
+        result = Kata.AddingShifted(arrayOfArrays, 3);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(string.Join(",", expected), string.Join(",", result));
+
+        arrayOfArrays = new int[][] { new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 7, 7, 7, -7, 7, 7 }, new int[] { 1, 1, 1, 1, 1, 1 } };
+        expected = new int[] { 1, 2, 3, 11, 12, 13, -6, 8, 8, 1, 1, 1 };
+        result = Kata.AddingShifted(arrayOfArrays, 3);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(string.Join(",", expected), string.Join(",", result));
     }
 }
 
