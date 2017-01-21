a8ea99e1096fce9cd89103e5656cb9314e74ef31
ChuckLu <244657538@qq.com>
2017-01-21 21:06:51 +0800

    https://www.codewars.com/kata/convert-a-number-to-a-string/train/javascript

--- JavaScript/Kata.js
+++ JavaScript/Kata.js
@@ -1,17 +1,4 @@
-/**
- * Created by Administrator on 2017/1/10.
- */
-function last(list)
+function numberToString(num)
 {
-    if(arguments.length==1)
-    {
-        var type = Object.prototype.toString.call(list);
-        console.log(type);
-        if(type=='[object Number]')
-        {
-            return list;
-        }
-        return list[list.length-1];
-    }
-    return arguments[arguments.length-1];
+    return num.toString();
 }
