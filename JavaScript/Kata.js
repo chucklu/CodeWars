87d175df5cfb48bde210c99a86b0b7b40dac084b
ChuckLu <244657538@qq.com>
2017-01-30 20:54:47 +0800

    将数组array放到函数外面

--- JavaScript/Kata.js
+++ JavaScript/Kata.js
@@ -1,6 +1,7 @@
+var array = ["I love you","a little","a lot","passionately","madly","not at all"];
+
 function howMuchILoveYou(nbPetals)
 {
-    var array = ["I love you","a little","a lot","passionately","madly","not at all"];
     var i = (nbPetals-1)%6;
     return array[i];
 }
