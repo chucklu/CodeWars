93e5d158d1795b0fdfd9a943a960078ed555a0e7
ChuckLu <244657538@qq.com>
2017-01-31 21:16:22 +0800

    https://www.codewars.com/kata/they-say-that-only-the-name-is-long-enough-to-attract-attention-they-also-said-that-only-a-simple-kata-will-have-someone-to-solve-it-this-is-a-sadly-story-number-1-are-they-opposite/train/javascript

--- JavaScript/Kata.js
+++ JavaScript/Kata.js
@@ -1,7 +1,32 @@
-var array = ["I love you","a little","a lot","passionately","madly","not at all"];
-
-function howMuchILoveYou(nbPetals)
+function isOpposite(s1,s2)
 {
-    var i = (nbPetals-1)%6;
-    return array[i];
+    if(s1 === s2)
+    {
+        return false;
+    }
+    if(s2.length > s1.length)
+    {
+        var s3 = s1;
+        s1 = s2;
+        s2 = s3;
+    }
+    var equal = true;
+    for (var item in s1)
+    {
+        var c= s1[item];
+        if(c == c.toUpperCase())
+        {
+            c = c.toLowerCase();
+        }
+        else
+        {
+            c = c.toUpperCase()
+        }
+        if(c != s2[item])
+        {
+            equal = false;
+            break;
+        }
+    }
+    return equal;
 }
