fc025b6bdc158c282981884a7ae75c971dcd016b
ChuckLu <244657538@qq.com>
2017-02-01 20:28:40 +0800

    https://www.codewars.com/kata/exclamation-marks-series-number-1-remove-a-exclamation-mark-from-the-end-of-string/train/javascript

--- JavaScript/Kata.js
+++ JavaScript/Kata.js
@@ -1,32 +1,5 @@
-function isOpposite(s1,s2)
+function remove(s)
 {
-    if(s1 === s2)
-    {
-        return false;
-    }
-    if(s2.length > s1.length)
-    {
-        var s3 = s1;
-        s1 = s2;
-        s2 = s3;
-    }
-    var equal = true;
-    for (var item in s1)
-    {
-        var c= s1[item];
-        if(c == c.toUpperCase())
-        {
-            c = c.toLowerCase();
-        }
-        else
-        {
-            c = c.toUpperCase()
-        }
-        if(c != s2[item])
-        {
-            equal = false;
-            break;
-        }
-    }
-    return equal;
+    s = s.replace(/\!$/, '');
+    return s;
 }
