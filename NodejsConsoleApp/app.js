0d18ce9138073c9fb27045a2adcbca47d664d8ec
Chuck Lu <chuck.lu@qq.com>
2019-01-11 16:54:55 +0800

    https://www.codewars.com/kata/capitalization-and-mutability/train/javascript

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -1,6 +1,16 @@
 'use strict';
 
-console.log('Hello world');
+console.log('start');
+
+var result = capitalizeWord('word');
+console.log(result);
+//https://www.codewars.com/kata/capitalization-and-mutability/train/javascript
+//https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+function capitalizeWord(word) {
+    return word[0].toUpperCase() + word.slice(1);
+}
+
+console.log('end');
 
 //https://www.codewars.com/kata/l1-set-alarm/train/javascript
 function setAlarm(employed, vacation) {
