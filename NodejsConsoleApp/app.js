471f1d375488a75dc0df9cbbd8adea01b81cf9f4
Chuck Lu <chuck.lu@qq.com>
2019-01-11 16:25:51 +0800

    https://www.codewars.com/kata/l1-set-alarm/train/javascript

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('Hello world');
+
+//https://www.codewars.com/kata/l1-set-alarm/train/javascript
+function setAlarm(employed, vacation) {
+    if (!employed) {
+        return false;
+    } else {
+        return !vacation;
+    }
+}
