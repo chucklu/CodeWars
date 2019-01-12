bffa0c8038e820de6092a3a4499ba5cb4444b2ff
Chuck Lu <chuck.lu@qq.com>
2019-01-12 18:32:30 +0800

    https://www.codewars.com/kata/total-amount-of-points/train/javascript

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -1,6 +1,37 @@
 'use strict';
 
 console.log('start');
+var array = ["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"];
+array = ["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"];
+points(array);
+//https://www.codewars.com/kata/total-amount-of-points/train/javascript
+//https://www.hacksparrow.com/global-variables-in-node-js.html
+function points(games) {
+    try {
+        global.total = 0;
+        games.map(calculate);
+        console.log(global.total);
+        return global.total;
+    }
+    catch (exception) {
+        console.log(exception);
+    }
+}
+
+function calculate(item) {
+    var array = item.split(":");
+    var x = array[0];
+    var y = array[1];
+    var result = 0;
+    if (x > y) {
+        result = 3;
+    } else if (x === y) {
+        result = 1;
+    }
+    global.total += result;
+}
+console.log('end');
+
 
 var result = capitalizeWord('word');
 console.log(result);
@@ -9,8 +40,6 @@ console.log(result);
 function capitalizeWord(word) {
     return word[0].toUpperCase() + word.slice(1);
 }
-
-console.log('end');
 
 //https://www.codewars.com/kata/l1-set-alarm/train/javascript
 function setAlarm(employed, vacation) {
