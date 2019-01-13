ec1bf72a067aa03df35b27816912e46f2619296e
Chuck Lu <chuck.lu@qq.com>
2019-01-13 13:20:19 +0800

    https://www.codewars.com/kata/exclamation-marks-series-number-6-remove-n-exclamation-marks-in-the-sentence-from-left-to-right/train/javascript

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,9 +2,21 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/reverse-list-order/train/javascript
-function reverseList(list) {
-    return list.reverse();
+//https://www.codewars.com/kata/exclamation-marks-series-number-6-remove-n-exclamation-marks-in-the-sentence-from-left-to-right/train/javascript
+function remove(s, n) {
+    var target = [];
+    for (var i = 0; i < s.length; i++) {
+        if (n > 0) {
+            if (s[i] !== '!') {
+                target.push(s[i]);
+            } else {
+                n--;
+            }
+        } else {
+            target.push(s[i]);
+        }
+    }
+    return target.join('');
 }
 
 console.log('end');
