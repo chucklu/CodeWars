09b371eecdcdd19a79ed20df0513e38e9e3dcf8d
Chuck Lu <chuck.lu@qq.com>
2019-01-14 13:05:16 +0800

    Number https://www.codewars.com/kata/parse-nice-int-from-char-problem/train/javascript https://stackoverflow.com/questions/1133770/convert-a-string-to-an-integer-in-javascript

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,12 +2,9 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/exclamation-marks-series-number-6-remove-n-exclamation-marks-in-the-sentence-from-left-to-right/train/javascript
-function remove(s, n) {
-    for (var i = 0; i < n; i++) {
-        s = s.replace('!', '');
-    }
-    return s;
+//https://www.codewars.com/kata/parse-nice-int-from-char-problem/train/javascript
+function getAge(inputString) {
+    return Number(inputString[0]);
 }
 
 console.log('end');
