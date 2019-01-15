ab4dfc20c88c400327638d1b06cfae6409627634
Chuck Lu <chuck.lu@qq.com>
2019-01-15 13:20:17 +0800

    https://stackoverflow.com/questions/3895478/does-javascript-have-a-method-like-range-to-generate-a-range-within-the-supp https://stackoverflow.com/questions/19544452/remove-last-item-from-array

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,9 +2,11 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/training-js-number-5-basic-data-types-object/train/javascript
-function animal(obj) {
-    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
-}
+https://www.codewars.com/kata/reversed-sequence/train/javascript
+const reverseSeq = n => {
+    var temp = [...Array(n + 1).keys()].reverse();
+    temp.pop();
+    return temp;
+};
 
 console.log('end');
