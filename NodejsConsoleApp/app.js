d24c3201c2ff904046058d2c3d877e9549fdd4d5
Chuck Lu <chuck.lu@qq.com>
2019-01-15 12:44:12 +0800

    https://www.codewars.com/kata/students-final-grade/train/javascript

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,9 +2,17 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/parse-nice-int-from-char-problem/train/javascript
-function getAge(inputString) {
-    return Number(inputString[0]);
+//https://www.codewars.com/kata/students-final-grade/train/javascript
+function finalGrade(exam, projects) {
+    var result = 0;
+    if (exam > 90 || projects > 10) {
+        result = 100;
+    } else if (exam > 75 && projects >= 5) {
+        result = 90;
+    } else if (exam > 50 && projects >= 2) {
+        result = 75;
+    }
+    return result;
 }
 
 console.log('end');
