60cfdba497d53ff91beb1def6a0397ee8108ec31
Chuck Lu <chuck.lu@qq.com>
2019-01-15 12:59:11 +0800

    string interpolation https://www.codewars.com/kata/training-js-number-5-basic-data-types-object/train/javascript

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,17 +2,9 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/students-final-grade/train/javascript
-function finalGrade(exam, projects) {
-    var result = 0;
-    if (exam > 90 || projects > 10) {
-        result = 100;
-    } else if (exam > 75 && projects >= 5) {
-        result = 90;
-    } else if (exam > 50 && projects >= 2) {
-        result = 75;
-    }
-    return result;
+//https://www.codewars.com/kata/training-js-number-5-basic-data-types-object/train/javascript
+function animal(obj) {
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
 
 console.log('end');
