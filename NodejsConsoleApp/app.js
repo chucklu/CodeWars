de433e75d2bdb1a3453a462a3b3e63072ec9f297
Chuck Lu <chuck.lu@qq.com>
2019-01-19 12:51:05 +0800

    string includes https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,20 +2,10 @@
 
 console.log('start');
 
-var result = sumMix([5]);
-console.log(result);
-
-//https://www.codewars.com/kata/sum-mixed-array/train/javascript
-function getSum(total, num) {
-    total = parseInt(total);
-    num = parseInt(num);
-    return total + num;
-}
-
-function sumMix(array) {
-    var total = array.reduce(getSum);
-    total = parseInt(total);
-    return total;
+//https://www.codewars.com/kata/do-you-speak-english/train/javascript
+function spEng(sentence) {
+    var str = sentence.toLowerCase();
+    return str.includes('english');
 }
 
 console.log('end');
