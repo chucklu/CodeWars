2fa59737b10b3d03b4ef91410a5e8425e3ccc1db
Chuck Lu <chuck.lu@qq.com>
2019-01-20 13:06:31 +0800

    check if the parameter of function is passed or not

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,10 +2,21 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/do-you-speak-english/train/javascript
-function spEng(sentence) {
-    var str = sentence.toLowerCase();
-    return str.includes('english');
+//https://www.codewars.com/kata/hello-name-or-world/train/javascript
+function hello(name) {
+    var result = '';
+    if (typeof name === "undefined") {
+        result = 'Hello, World!';
+    } else {
+        if (name === '') {
+            result = 'Hello, World!';
+        } else {
+            var str = name.toLowerCase();
+            str = str.charAt(0).toUpperCase() + str.slice(1);
+            result = `Hello, ${str}!`;
+        }
+    }
+    return result;
 }
 
 console.log('end');
