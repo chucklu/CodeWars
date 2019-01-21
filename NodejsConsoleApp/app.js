ae9dcb18c55dbd37cfa4be1a0dfdf118aa3e7bbd
Chuck Lu <chuck.lu@qq.com>
2019-01-21 13:16:24 +0800

    usage of includes

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,21 +2,9 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/hello-name-or-world/train/javascript
-function hello(name) {
-    var result = '';
-    if (typeof name === "undefined") {
-        result = 'Hello, World!';
-    } else {
-        if (name === '') {
-            result = 'Hello, World!';
-        } else {
-            var str = name.toLowerCase();
-            str = str.charAt(0).toUpperCase() + str.slice(1);
-            result = `Hello, ${str}!`;
-        }
-    }
-    return result;
+//https://www.codewars.com/kata/you-only-need-one-beginner/train/javascript
+function check(a, x) {
+    return a.includes(x);
 }
 
 console.log('end');
