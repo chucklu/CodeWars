afffc736cefca2b2ff49f41158fb8b039dc2fdb0
Chuck Lu <chuck.lu@qq.com>
2019-01-22 13:15:57 +0800

    usage of Date

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,9 +2,12 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/you-only-need-one-beginner/train/javascript
-function check(a, x) {
-    return a.includes(x);
+//https://www.codewars.com/kata/is-the-date-today/train/javascript
+function isToday(date) {
+    var today = new Date();
+    var date1 = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    var date2 = new Date(Date.UTC(today.getFullYear(), today.getMonth(), today.getDate()));
+    return date1.getTime() === date2.getTime();
 }
 
 console.log('end');
