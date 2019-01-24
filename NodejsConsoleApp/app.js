719e49c8ac532b9e053c304e990b29520ae3b106
Chuck Lu <chuck.lu@qq.com>
2019-01-24 13:28:49 +0800

    https://www.codewars.com/kata/twice-as-old/train/javascript

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,12 +2,9 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/abbreviate-a-two-word-name/train/javascript
-function abbrevName(name) {
-    var array = name.split(' ');
-    var first = array[0].charAt(0).toUpperCase();
-    var last = array[1].charAt(0).toUpperCase();
-    return `${first}.${last}`;
+//https://www.codewars.com/kata/twice-as-old/train/javascript
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
 
 console.log('end');
