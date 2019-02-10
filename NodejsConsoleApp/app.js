1ac5da510065bdf22a69b2da62446563dab1ee30
Chuck Lu <chuck.lu@qq.com>
2019-02-10 13:00:17 +0800

    Return the day

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,14 +2,33 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/is-this-my-tail/train/javascript
-function correctTail(body, tail) {
-    var sub = body.substr(body.length - tail.length);
-    if (sub === tail) {
-        return true;
-    } else {
-        return false;
+//https://www.codewars.com/kata/return-the-day/train/javascript
+function whatday(num) {
+    var result = 'Wrong, please enter a number between 1 and 7';
+    switch (num) {
+    case 1:
+        result = "Sunday";
+        break;
+    case 2:
+        result = "Monday";
+        break;
+    case 3:
+        result = "Tuesday";
+        break;
+    case 4:
+        result = "Wednesday";
+        break;
+    case 5:
+        result = "Thursday";
+        break;
+    case 6:
+        result = "Friday";
+        break;
+    case 7:
+        result = "Saturday";
+        break;
     }
+    return result;
 }
 
 console.log('end');
