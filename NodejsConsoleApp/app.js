06ae9912f2332583b78476d4a525e9ca277df5d2
Chuck Lu <chuck.lu@qq.com>
2019-02-28 13:12:55 +0800

    Who is going to pay for the wall?

--- NodejsConsoleApp/app.js
+++ NodejsConsoleApp/app.js
@@ -2,9 +2,13 @@
 
 console.log('start');
 
-//https://www.codewars.com/kata/returning-strings/train/javascript
-function greet(name) {
-    return `Hello, ${name} how are you doing today?`;
+//https://www.codewars.com/kata/who-is-going-to-pay-for-the-wall/train/javascript
+function whoIsPaying(name) {
+	if (name.length <= 2) {
+		return [name];
+	} else {
+		return [name, name.substring(0, 2)];
+	}
 }
 
 console.log('end');
